35f0a596a63ed6f5fdb38a1b3e8ffa27f2a4d37d
ElementAyush <ele.ayush@hotmail.com>
2018-09-21 19:42:08 +0530

    Folder merger

--- OnlineJudges/Hackerrank/sherlock-and-cost.cpp
+++ OnlineJudges/Hackerrank/sherlock-and-cost.cpp
@@ -2,36 +2,27 @@
 
 using namespace std ;
 
-vector<int> b ;
-int n ;
-
-int solve()
-{
-	int hi = 0 , lo = 0 ;
-	for(int i = 1 ; i < n ; i++)
-	{
-		max(b[])
-	}
-}
-
 int main()
 {
-	int test_cases ;
+	int test_cases , n ;
+	vector<int> vec ;
+	
 	cin >> test_cases ;
 	
 	while(test_cases--)
 	{
-		
 		cin >> n ;
 		int k ;
 		for(int i = 0 ; i < n ; i++)
-		{
-			cin >> k ;
-			b.push_back(k) ;
-		}
-		
-		solve() ;
-		b.clear() ;
+		 {
+		 	cin >> k ;
+		 	vec.push_back(k) ;
+		 }
+		 
+		 int hi = 0  , lo = 0 ; 
+		 for(int i = 1 ; i < n ; i++)
+		 {
+		 	high_to_low = max(hi , hi + vec[i])
+		 }
 	}
-	
 }
